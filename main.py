ca6dec7bf43a9883397bbee1eea04689966eb145
sem_wett <90400355+semwett0301@users.noreply.github.com>
2022-04-27 22:09:59 +0300

    First part

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
+import numpy as np
+from termcolor import colored
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+selection = np.array([-0.34, -0.73, 0.69, 0.55, 1.11, 0.62, 0.93, 0.42, 1, -0.48, -1.1, -0.66, -0.93, 0.42, -0.22, -0.11, -1.06, -0.72, -0.1, 1.05])
+sort_selection = np.sort(selection)
+
+print('Вариационный ряд:\n' + colored(str(sort_selection), 'green'))
+print('Наименьшее значение (первая порядковая статистика)\n' + colored(str(sort_selection[0]), 'green'))
+print('Наибольшее значение (n-ая порядковая статистика)\n' + colored(str(sort_selection[sort_selection.size - 1]), 'green'))
+print('Размах\n' + colored(str(sort_selection[sort_selection.size - 1] - sort_selection[0]), 'green'))
+
+mathematical_expectation = np.sum(sort_selection) / 20
+average_deviation = (np.sum(sort_selection ** 2) / 20 - mathematical_expectation ** 2) ** 0.5
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
